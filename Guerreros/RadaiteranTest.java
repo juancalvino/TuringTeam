1f07b723b6b5558ead492705758a62470538b3af
Franco <SpataroFranco1@Gmail.com>
2021-05-23 20:51:39 -0300

    Se agrega Clase RADAITERAN con Test Unitarios

---
+++ Guerreros/RadaiteranTest.java
@@ -0,0 +1,92 @@
+package Guerreros;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class RadaiteranTest {
+
+	// Prueba crear guerrero Radaiteran
+	@Test
+	public void prueba000() {
+		Radaiteran gRadaiteran = new Radaiteran();
+	}
+
+	// Prueba corroborar valores predeterminados del guerrero
+	@Test
+	public void prueba001() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		Assert.assertEquals(36, gRadaiteran.getSalud());
+		Assert.assertEquals(56, gRadaiteran.getDanioBasico());
+	}
+
+	// Prueba ataque Radaiteran
+	@Test
+	public void prueba002() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		GuerreroRandom guerrero = new GuerreroRandom(200, 20);
+		gRadaiteran.atacar(guerrero);
+		Assert.assertEquals(144, guerrero.getSalud());
+	}
+
+	// Prueba ataque doble Radaiteran
+	@Test
+	public void prueba003() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		GuerreroRandom guerrero = new GuerreroRandom(200, 20);
+		gRadaiteran.atacar(guerrero);
+		gRadaiteran.atacar(guerrero);
+		Assert.assertEquals(85, guerrero.getSalud());
+	}
+
+	// Prueba ataque quintuple Radaiteran
+	@Test
+	public void prueba004() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		GuerreroRandom guerrero = new GuerreroRandom(500, 20);
+		gRadaiteran.atacar(guerrero);
+		gRadaiteran.atacar(guerrero);
+		gRadaiteran.atacar(guerrero);
+		gRadaiteran.atacar(guerrero);
+		gRadaiteran.atacar(guerrero);
+		Assert.assertEquals(190, guerrero.getSalud());
+	}
+
+	// Prueba recibir ataque
+	@Test
+	public void prueba005() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		GuerreroRandom guerrero = new GuerreroRandom(100, 20);
+		guerrero.atacar(gRadaiteran);
+		Assert.assertEquals(16, gRadaiteran.getSalud());
+	}
+
+	// Prueba recibir ataque triple
+	@Test
+	public void prueba006() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		GuerreroRandom guerrero = new GuerreroRandom(100, 5);
+		guerrero.atacar(gRadaiteran);
+		guerrero.atacar(gRadaiteran);
+		guerrero.atacar(gRadaiteran);
+		Assert.assertEquals(21, gRadaiteran.getSalud());
+	}
+
+	// Prueba descansar
+	@Test
+	public void prueba007() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		gRadaiteran.descansar();
+		Assert.assertFalse(gRadaiteran.isActivo());
+	}
+
+	// Prueba descansar y despertar
+	@Test
+	public void prueba008() {
+		Radaiteran gRadaiteran = new Radaiteran();
+		gRadaiteran.descansar();
+		gRadaiteran.setActivo(true);
+		Assert.assertTrue(gRadaiteran.isActivo());
+	}
+
+}
