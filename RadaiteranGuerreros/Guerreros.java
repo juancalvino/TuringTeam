b6d5e6d760f490b25fb7221aadcbd6203325dbc2
Juan Manuel Calviño <30604398+juancalvino@users.noreply.github.com>
2021-05-28 17:43:29 -0300

    Se elimina Radaiteran version vieja

--- RadaiteranGuerreros/Guerreros.java
+++
@@ -1,9 +0,0 @@
-package Guerreros;
-
-public abstract class Guerreros {
-
-	public abstract int getSalud(); 
-	public abstract void descansar();
-	public abstract void atacar(Guerreros enemigo);
-	public abstract void recibirAtaque (int ataqueEnemigo);
-}
