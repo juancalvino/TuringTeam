377eb6a85afbc7c71c2ed7ad1e3903d62957fff1
Franco <SpataroFranco1@Gmail.com>
2021-06-09 17:08:00 -0300

    Update EjercitoTest

--- test/combatientes/ejercito/EjercitoTest.java
+++ test/combatientes/ejercito/EjercitoTest.java
@@ -4,8 +4,11 @@ package combatientes.ejercito;
 import org.junit.Assert;
 import org.junit.Test;
 
+import combatientes.guerrero.GuerreroHack;
+import combatientes.guerrero.Nortaichian;
 import combatientes.guerrero.Reralopes;
 import combatientes.guerrero.Wrives;
+import escenario.Batalla;
 
 public class EjercitoTest {
 
@@ -54,4 +57,63 @@ public class EjercitoTest {
 		Assert.assertEquals(jugador.getSalud(), 108 );
 
 	}	
+	@Test
+	public void descansarEjercitoDeNortaichian() {
+		// Cuando descansa, recupera toda su salud
+		
+		EjercitoJugador jugador = new EjercitoJugador(new Nortaichian(), 2);
+		EjercitoEnemigo enemigo = new EjercitoEnemigo(new GuerreroHack(20, 20), 1);
+		
+		Batalla batallar = new Batalla();
+		Assert.assertEquals(batallar.obtenerSobreviviente(jugador, enemigo), jugador);
+		
+		Assert.assertEquals(jugador.getSalud(), 49);
+		
+		EjercitoEnemigo otroEnemigo = new EjercitoEnemigo(new GuerreroHack(40, 67), 1);
+		Assert.assertEquals(batallar.obtenerSobreviviente(jugador, otroEnemigo), jugador);
+		
+		Assert.assertEquals(jugador.cantidadDeGuerreros(), 1);
+		Assert.assertEquals(jugador.getSalud(), 1);
+		
+		jugador.descansar();
+		Assert.assertEquals(jugador.getSalud(), 66);
+	}
+
+	@Test
+	public void descansarEjercitoWrives() {
+		// Cuando descansa aumenta su salud y su salud máxima en 50.
+
+		EjercitoJugador jugador = new EjercitoJugador(new Wrives(), 2);
+		EjercitoEnemigo enemigo = new EjercitoEnemigo(new GuerreroHack(300, 50), 1);
+
+		Batalla batallar = new Batalla();
+		Assert.assertEquals(batallar.obtenerSobreviviente(jugador, enemigo), jugador);
+		
+		Assert.assertEquals(jugador.getSalud(), 8);
+		
+		jugador.descansar();
+		Assert.assertEquals(jugador.getSalud(), 58);
+	}
+	
+	@Test
+	public void descansarEjercitoReralopes() {
+		//Cuando descansa, se concentra y sus próximos 3 ataques (de esa unidad) dañan el doble del valor correspondiente
+		
+		EjercitoJugador jugador = new EjercitoJugador(new Reralopes(), 2);
+		EjercitoEnemigo enemigo = new EjercitoEnemigo(new GuerreroHack(200, 30), 1);
+		
+		enemigo.recibirAtaque(jugador.atacar());
+		enemigo.recibirAtaque(jugador.atacar());
+		Assert.assertEquals(enemigo.getSalud(), 146);
+		
+		//ataques errados
+		enemigo.recibirAtaque(jugador.atacar()); enemigo.recibirAtaque(jugador.atacar());
+		
+		jugador.descansar();		
+		
+		enemigo.recibirAtaque(jugador.atacar());
+		enemigo.recibirAtaque(jugador.atacar());
+		enemigo.recibirAtaque(jugador.atacar());
+		Assert.assertEquals(enemigo.getSalud(), 38);	
+	}
 }
