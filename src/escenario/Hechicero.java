eea2a84f5da12d8dcf3ebea2baf867c567f0c1fe
Juan Calvino <juan.m.calvino@gmail.com>
2021-06-18 18:59:06 -0300

    Hechicero

--- src/escenario/Hechicero.java
+++ src/escenario/Hechicero.java
@@ -5,11 +5,14 @@ import java.util.Stack;
 
 public class Hechicero {
 
-	 HashMap<String, Pueblo> vertices = Escenario.getInstance().getMapaDePueblos();
+	 HashMap<String, Pueblo> vertices;
 	
 	public HashMap<String, String> dfs(String inicio) {
 		HashMap<String, String> predecesores = new HashMap<String, String>();
 		Stack<String> pila = new Stack<String>();
+		
+		vertices = Escenario.getInstance().getMapaDePueblos();
+		
 		for (String vertice : vertices.keySet()) {
 			predecesores.put(vertice, null);
 		}
@@ -25,6 +28,7 @@ public class Hechicero {
 		}
 		return predecesores;
 	}
+	
 	
 
 
